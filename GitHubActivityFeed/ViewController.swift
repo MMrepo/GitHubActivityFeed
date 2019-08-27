34f867c41a248d1b3c2d1bd05072fbd135c84104
Mateusz Małek <it.mateuszmalek@gmail.com>
2019-08-27 11:31:39 +0200

    Initial Commit

---
+++ GitHubActivityFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubActivityFeed
+//
+//  Created by Mateusz Małek on 27/08/2019.
+//  Copyright © 2019 Mateusz Małek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ GitHubActivityFeedTests/GitHubActivityFeedTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHubActivityFeedTests.swift
+//  GitHubActivityFeedTests
+//
+//  Created by Mateusz Małek on 27/08/2019.
+//  Copyright © 2019 Mateusz Małek. All rights reserved.
+//
+
+import XCTest
+@testable import GitHubActivityFeed
+
+class GitHubActivityFeedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

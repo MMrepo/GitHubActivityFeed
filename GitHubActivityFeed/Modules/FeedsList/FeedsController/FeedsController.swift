f63a3188612acd6d7d33b7fe22db0b650a6af154
Mateusz Małek <it.mateuszmalek@gmail.com>
2019-08-27 12:16:35 +0200

    Add FeedsController

---
+++ GitHubActivityFeed/Modules/FeedsList/FeedsController/FeedsController.swift
@@ -0,0 +1,9 @@
+//
+//  FeedsController.swift
+//  GitHubActivityFeed
+//
+//  Created by Mateusz Małek on 27/08/2019.
+//  Copyright © 2019 Mateusz Małek. All rights reserved.
+//
+
+import Foundation
